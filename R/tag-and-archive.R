d315c7eef8e4286f7f893c4a175debea41f078e4
Luke W. Johnston <lwjohnst@gmail.com>
2022-05-02 11:33:43 +0200

    add authors, r script to make applicatio html, and application file

---
+++ R/tag-and-archive.R
@@ -0,0 +1,35 @@
+# library(tidyverse)
+library(here)
+library(gert)
+
+# Generate HTML of application for archiving -------------------------------
+# 1. Comment out the d-title element in the theme.css
+# 2. Comment out the navbar in the _site.yml
+
+rmarkdown::render(
+    here("index.Rmd"),
+    output_format = distill::distill_article(self_contained = TRUE, toc = TRUE, toc_float = FALSE),
+    output_file = here("nnf-dif-application.html")
+)
+
+# Then add and commit to the Git repo.
+
+repo_version <- "v2021.05.04"
+version_tag <- git_tag_create(
+    name = repo_version,
+    message = "Grant application submitted to Novo Nordisk Foundation for the Data Science Infrastructure funding. This is the version submitted to NNF, even though the tag date is off by a year."
+)
+git_push()
+git_tag_push(repo_version)
+if (interactive()) browseURL("https://gitlab.com/rostools/r-cubed/-/releases/new")
+
+# This is for when we upload to Zenodo
+# tag_archive_file <- str_c("dif-project", repo_version, ".zip")
+# git_archive_zip(tag_archive_file)
+
+# zenodo <- ZenodoManager$new(
+#     # url = "https://sandbox.zenodo.org/api",
+#     url = "https://zenodo.org/api",
+#     logger = "INFO",
+#     token = askpass::askpass()
+# )
