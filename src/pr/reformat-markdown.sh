d58a5ef8f1447e21d4b7cefc8783b030fa98fbc2
Luke W. Johnston <lwjohnst@gmail.com>
2023-03-15 16:51:55 +0100

    fix to find q,rmd files and use pandoc directly (if isn't necessary)

--- src/pr/reformat-markdown.sh
+++ src/pr/reformat-markdown.sh
@@ -4,19 +4,17 @@
 # (relative to main) and reformats them to "canonical" Markdown
 # format. Used for the `/reformat_markdown` Pull Request command,
 # found in `.github/workflows/pr-commands.yaml`.
-for file in $(git diff --name-only --relative main... | cat); do
+for file in $(git diff --name-only --relative main... | cat | egrep "\.[qRr]?md$"); do
   echo "- The file '$file' was modified in this branch."
-  if [[ $file =~ \.[qRr]?md$ ]]; then
-    # https://github.com/rstudio/rstudio/blob/4f7063b13bb0bae8377639a36d3ff4b7626de6e3/src/gwt/panmirror/src/editor/src/pandoc/pandoc_converter.ts#L90
-    pandoc \
-      --from markdown-smart+yaml_metadata_block+raw_attribute+blank_before_header \
-      --standalone \
-      --markdown-headings=atx \
-      --wrap=auto \
-      --columns=72 \
-      --to markdown-smart+yaml_metadata_block+raw_attribute+blank_before_header \
-      -o $file \
-      $file
-    echo "  - '$file' was reformatted to canonical markdown."
-  fi
+  # https://github.com/rstudio/rstudio/blob/4f7063b13bb0bae8377639a36d3ff4b7626de6e3/src/gwt/panmirror/src/editor/src/pandoc/pandoc_converter.ts#L90
+  pandoc \
+    --from markdown-smart+yaml_metadata_block+raw_attribute+blank_before_header \
+    --standalone \
+    --markdown-headings=atx \
+    --wrap=auto \
+    --columns=72 \
+    --to markdown-smart+yaml_metadata_block+raw_attribute+blank_before_header \
+    -o $file \
+    $file
+  echo "  - '$file' was reformatted to canonical markdown."
 done
