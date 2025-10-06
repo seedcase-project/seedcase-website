29479d80ddf23a9af5ba5d7ba52b90fb7406a67e
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2025-10-06 09:53:51 +0200

    chore: 👩‍💻 add `sh` helper script that gets team members' GitHub avatars (#370)

---
+++ tools/get-github-avatars-helper.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# This script is used to download the profile pictures (avatars) of our team members,
+# to be used on our About page. It saves the images in the "about/images/" directory.
+
+USERNAMES=("lwjohnst86" "k-beicher" "martonvago" "signekb")
+SAVE_DIR="./about/images"
+
+for USER in "${USERNAMES[@]}"; do
+  AVATAR_URL=$(curl -s "https://api.github.com/users/$USER" | jq -r .avatar_url)
+  curl -s "$AVATAR_URL" -o "$SAVE_DIR/${USER}.jpg"
+  echo "Saved $USER's avatar to $SAVE_DIR/${USER}.jpg"
+done
